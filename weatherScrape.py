d45a440e4b461a8096173957d0a8d7475aa7b719
Elizabeth Paperno <liza.paperno@gmail.com>
2024-05-20 23:13:58 -0400

    clean up data (date, temp), add primary key

--- weatherScrape.py
+++ weatherScrape.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 from supabase import create_client, Client
+from datetime import datetime
 
 SUPABASE_URL = "https://kdpfszinopwchmjvxdfl.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkcGZzemlub3B3Y2htanZ4ZGZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYyNDM0OTgsImV4cCI6MjAzMTgxOTQ5OH0.H6fs_uUEeOHjscCpqwD3MSSvLeMvsfaO9vOlbo0EDsY"
@@ -25,12 +26,12 @@ def scrape_weather_data(location_code):
 
     location = soup.find('h1', {'class': 'CurrentConditions--location--1YWj_'}).text
     
-    curr_date = date.today()
+    curr_date = datetime.now().strftime("%Y-%m-%d")
 
-    dict["curr_date"] = date
+    dict["curr_date"] = curr_date
     dict["location"] = location
-    dict["high"] = high_temp
-    dict["low"] = low_temp
+    dict["high"] = format_temp(high_temp)
+    dict["low"] = format_temp(low_temp)
 
     return dict
 
@@ -40,13 +41,23 @@ def scrape_weather_data(location_code):
     # print(f"High Temperature: {high_temp}")
     # print(f"Low Temperature: {low_temp}")
 
-def update_tables(client, dict):
+def format_temp(unformatted_temp):
+    if (unformatted_temp == "--"):
+        formatted_temp = None
+    else:
+        # get rid of degree sign and cast to int
+        formatted_temp = int(unformatted_temp[:-1])
+    return formatted_temp
+
+def update_tables(count, client, dict):
     #data, count = supabase.table('Date').insert({"date": dict.get("date")}).execute()
     #data, count = supabase.table('Location').insert({"location": dict.get("location")}).execute()
-    data, count = client.table('weather').insert({"date": dict.get("date"), "location": dict.get("location"), "high": dict.get("high"), "low": dict.get("low")}).execute()
+    data, count = client.table('weather').insert({"id": count,"date": dict.get("date"), "location": dict.get("location"), "high": dict.get("high"), "low": dict.get("low")}).execute()
 
 if __name__ == "__main__":
     supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+    count = 0
     for location_code in location_codes:
         dict = scrape_weather_data(location_code)
-        update_table(supabase, dict)
+        update_table(count, supabase, dict)
+        count += 1; 
