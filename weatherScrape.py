57ed541d139de75aae6e61963a5bf983a29b8371
Elizabeth Paperno <liza.paperno@gmail.com>
2024-05-20 22:05:24 -0400

    get high and low temp data

--- weatherScrape.py
+++ weatherScrape.py
@@ -5,4 +5,13 @@ location_codes = {"USNY0002:1:US"}
 
 page = requests.get("https://weather.com/weather/today/l/USNY0002:1:US")
 soup = BeautifulSoup(page.content, "html.parser")
-print(soup)
+# print(soup)
+
+wx_data_div = soup.find('div', {'data-testid': 'wxData', 'class': 'WeatherDetailsListItem--wxData--kK35q'})
+temperatures = wx_data_div.find_all('span', {'data-testid': 'TemperatureValue'})
+high_temp = temperatures[0].text
+low_temp = temperatures[1].text
+
+# print extracted high and low temp
+print(f"High Temperature: {high_temp}")
+print(f"Low Temperature: {low_temp}")
