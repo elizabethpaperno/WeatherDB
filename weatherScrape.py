5ea993df8a15eba07e40508290193320db9fde0a
Elizabeth Paperno <liza.paperno@gmail.com>
2024-05-22 16:21:57 -0400

    add most doc-strings, still need to refactor location table

--- weatherScrape.py
+++ weatherScrape.py
@@ -11,10 +11,7 @@ SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJ
 # 10 chosen location codes
 location_codes = {"USNY0002:1:US", "USFL0002:1:US", "USNJ0002:1:US", "USVT0002:1:US", "USMA0002:1:US", "USNH0002:1:US", "USVA0002:1:US", "USCA0002:1:US", "USCO0002:1:US", "USUT0002:1:US"}
 
-def get_todays_date():
-    return datetime.now().strftime("%Y-%m-%d"); 
-
-def scrape_weather_data(client, location_code):
+def scrape_weather_data(client, date, location_code):
     page = requests.get(f"https://weather.com/weather/today/l/{location_code}")
     soup = BeautifulSoup(page.content, "html.parser")
 
@@ -40,21 +37,105 @@ def scrape_weather_data(client, location_code):
 
 # Functions that update each table
 def update_weather_table(client, date, location_code, high_temp, low_temp):
+    """
+    Insert row to weather table containing date_id, location_code, high_temp, and low_temp
+
+    Parameters
+    -------
+    client: Client 
+        Connection to supabase database
+    date: string
+        Date associated with date_id
+    location_code: string
+        Location code 
+    high_temp: int
+        High temp per location per date
+    low_temp: int
+        Low temp per location per date
+
+    Returns
+    -------
+    void
+    """
     date_id = get_date_id(client, date)
     response = client.table('weather_table').insert({"date_id": date_id, "location_code": location_code, "high": high_temp, "low": low_temp}).execute()
 
 def update_location_table(client, location_code, location_name): 
-    data, count = client.table('location_table').insert({"location_id": location_code, "location_name": location_name}).execute()
+    """
+    Insert row to location table containing location_code and location_name
+
+    Parameters
+    -------
+    client: Client 
+        Connection to supabase database
+    location_code: string
+    location_name: string
+
+    Returns
+    -------
+    void
+    """
+    data, count = client.table('location_table').insert({"location_code": location_code, "location_name": location_name}).execute()
 
 def update_date_table(client, date): 
+    """
+    Insert row to location table containing date (date_id is autoincremented)
+    Parameters
+    -------
+    client: Client 
+        Connection to supabase database
+    date: string
+
+    Returns
+    -------
+    void
+    """
     data, count = client.table('date_table').insert({"date": date}).execute()
 
 # Helper functions
+def get_todays_date():
+    """
+    Get today's date. 
+
+    Returns
+    -------
+    string
+        Today's date in "%Y-%m-%d" format
+    """
+    return datetime.now().strftime("%Y-%m-%d"); 
+
 def get_date_id(client, date):
+    """
+    Get date_id assoicated with ``date``. 
+
+    Parameters
+    -------
+    client: Client 
+        Connection to supabase database
+    date: string
+        string representation of date
+
+    Returns
+    -------
+    int
+        date_id associated with ``date``
+    """
     data,count = client.from_('date_table').select('date_id').eq('date', date).execute()
     return int(data[1][0].get("date_id"))
 
 def format_temp(unformatted_temp):
+    """
+    Format string representation of temperature.   
+
+    Parameters
+    -------
+    unformatted_temp: string 
+
+    Returns
+    -------
+    int
+        formatted integer representation of temperature
+    """
     if (unformatted_temp == "--"):
         formatted_temp = None
     else:
@@ -67,4 +148,4 @@ if __name__ == "__main__":
     date = get_todays_date()
     update_date_table(supabase, date)
     for location_code in location_codes:
-        scrape_weather_data(supabase, location_code)
+        scrape_weather_data(supabase, date, location_code)
