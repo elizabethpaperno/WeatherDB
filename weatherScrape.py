db082e351f65163bbe05d93e0cfc7d969c7e0bef
Elizabeth Paperno <liza.paperno@gmail.com>
2024-05-22 22:10:07 -0400

    refactor location table to only be populated once

--- weatherScrape.py
+++ weatherScrape.py
@@ -9,9 +9,9 @@ SUPABASE_URL = "https://kdpfszinopwchmjvxdfl.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkcGZzemlub3B3Y2htanZ4ZGZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYyNDM0OTgsImV4cCI6MjAzMTgxOTQ5OH0.H6fs_uUEeOHjscCpqwD3MSSvLeMvsfaO9vOlbo0EDsY"
 
 # 10 chosen location codes
-location_codes = {"USNY0002:1:US", "USFL0002:1:US", "USNJ0002:1:US", "USVT0002:1:US", "USMA0002:1:US", "USNH0002:1:US", "USVA0002:1:US", "USCA0002:1:US", "USCO0002:1:US", "USUT0002:1:US"}
+location_codes = []
 
-def scrape_weather_data(client, date, location_code):
+def scrape_weather_data_update_weather_table(client, date, location_code):
     page = requests.get(f"https://weather.com/weather/today/l/{location_code}")
     soup = BeautifulSoup(page.content, "html.parser")
 
@@ -23,17 +23,24 @@ def scrape_weather_data(client, date, location_code):
     temperatures = soup.find_all('span', {'data-testid': 'TemperatureValue'})
 
     high_temp = temperatures[0].text
-    low_temp = temperatures[1].text
-
-    location = soup.find('h1', {'class': 'CurrentConditions--location--1YWj_'}).text  
-
-    try: 
-        update_location_table(client, location_code, location)  
-    except: 
-         # Means location has already been added, ignore error
-        pass
+    low_temp = temperatures[1].text  
 
     update_weather_table(client, date, location_code, format_temp(high_temp), format_temp(low_temp))
+
+def init_location_table(client, location_codeslcl):
+    for location_code in location_codeslcl:
+        page = requests.get(f"https://weather.com/weather/today/l/{location_code}")
+        soup = BeautifulSoup(page.content, "html.parser")
+
+        location = soup.find('h1', {'class': 'CurrentConditions--location--1YWj_'}).text
+        update_location_table(client, location_code, location)
+
+def populate_global_location_codes(client):
+    data,count = client.from_('location_table').select('location_code').execute()
+    for i in range(len(data[1])):
+        print(data[1][i].get("location_code"))
+        location_codes.append(data[1][i].get("location_code"))
+    print(location_codes)
 
 # Functions that update each table
 def update_weather_table(client, date, location_code, high_temp, low_temp):
@@ -143,9 +150,15 @@ def format_temp(unformatted_temp):
         formatted_temp = int(unformatted_temp[:-1])
     return formatted_temp
 
+# Run once
+def setup(client):
+    init_location_table(client, ["USNY0002:1:US", "USFL0002:1:US", "USNJ0002:1:US", "USVT0002:1:US", "USMA0002:1:US", "USNH0002:1:US", "USVA0002:1:US", "USCA0002:1:US", "USCO0002:1:US", "USUT0002:1:US"])
+
+# Ran daily
 if __name__ == "__main__":
     supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
     date = get_todays_date()
     update_date_table(supabase, date)
+    populate_global_location_codes(supabase)
     for location_code in location_codes:
-        scrape_weather_data(supabase, date, location_code)
+        scrape_weather_data_update_weather_table(supabase, date, location_code)
